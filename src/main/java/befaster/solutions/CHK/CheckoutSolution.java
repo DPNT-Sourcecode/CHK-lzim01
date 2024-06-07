47e81513d46421cecd28be96072a2c0e021be505
sbx_user1051 <sbx_user1051@169.254.63.117>
2024-06-07 11:40:56 +0000

    Fri Jun 07 11:40:56 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -57,7 +57,7 @@ public class CheckoutSolution {
         }
         if(freeB > 0){
             if(totalB >= freeB){
-                total = total - freeB * 30 + freeB / 2 * 15;
+                total = total - freeB * 30 + (totalB - freeB) / 2 * 15;
             }
             else{
                 total = total - totalB * 30 + totalB / 2 * 15;
@@ -67,6 +67,7 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
 
